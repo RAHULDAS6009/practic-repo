1898a9113e99ac8516146342da151a2921090822
Rahul Das <121535378+RAHULDAS6009@users.noreply.github.com>
2024-10-17 02:18:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-
+const cors=require("cors");
+app.use(cors());
 function getRandomNumber(max) {
   return Math.floor(Math.random() * max);
 }
